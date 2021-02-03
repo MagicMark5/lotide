b4217f1c175eb6d3b50fdd8d9224e553429a64b0
Adam Marsala <adammarsala@hotmail.com>
2021-02-03 01:41:58 +0000

    without function passed all assertion checks

--- without.js
+++ without.js
@@ -1,10 +1,30 @@
 const without = function(source, itemsToRemove) {
-  // This function should take in a source array and a itemsToRemove array. 
+  // This function should take in a source array and a itemsToRemove array.
   // It should return a new array with only those elements from source
   // that are not present in the itemsToRemove array.
+  let indexToRemove; // contains index numbers to remove from source
+  let withoutArray = []; // will contain final array to return
 
-}
+  for (let i = 0; i < source.length; i++) {
 
+    for (item of itemsToRemove) { // compare each element of outer loop with itemsToRemove
+      if (source[i] === item) {
+        indexToRemove = i; // store indices which need to be removed from each iteration of outer loop
+      }
+    }
+
+    if (source[i] !== source[indexToRemove]) {
+      withoutArray.push(source[i]);
+    }
+    
+  }
+
+  return withoutArray;
+};
+
+
+
+// assert Arrays are equal by calling eqArrays
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -12,6 +32,8 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
 
 // compare 2 arrays and return a boolean if they are strictly equal
 const eqArrays = function(arr1, arr2) {
@@ -39,11 +61,13 @@ const eqArrays = function(arr1, arr2) {
 
 
 // TEST CODE
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 // assertArraysEqual test cases
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => Should PASS
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => Should PASS
+assertArraysEqual(without([12, 23, 43, 50, "dog"], ["12", 50, 0, "dog"]), [12, 23, 43]); // should pass
+assertArraysEqual(without([12, 23], []), [12, 23]); // should pass
 // Make sure the original array was not altered by the without function
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);

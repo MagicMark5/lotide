ed60ff08d9111439a06cdcaec02cbd76a0db777a
Adam Marsala <adammarsala@hotmail.com>
2021-02-04 00:37:29 +0000

    Refactored to skip whitespaces in strings instead of remove them

--- letterPositions.js
+++ letterPositions.js
@@ -34,27 +34,19 @@ const makeLowerCase = function(string) {
   return string.toLowerCase();
 };
 
-const removeWhiteSpace = function(string) {
-  let noSpaceString = "";
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] !== " ") {
-      noSpaceString += string[i];
-    }
-  }
-  return noSpaceString;
-};
-
 const letterPositions = function(sentence) {
   const characterIndices = {};
-  const sentenceStr = makeLowerCase(removeWhiteSpace(sentence)); // First call helper functions to process string
+  const sentenceStr = makeLowerCase(sentence); // First call helper functions to process string
   // Loop through each character of the sentence string argument
   // Add a property and update array index values of characterIndices object on each iteration
   for (let i = 0; i < sentenceStr.length; i++) {
-    let char = sentenceStr[i]; // store a temp variable "char" to hold the current character to use as a key name
-    if (characterIndices[char]) {
-      characterIndices[char].push(i); // if the character already exists in the object, push the index to it
-    } else {
-      characterIndices[char] = [i]; // if the character does not exist in the count, give it an array with that index
+    if (sentenceStr[i] !== " ") {
+      let char = sentenceStr[i]; // store a temp variable "char" to hold the current character to use as a key name
+      if (characterIndices[char]) {
+        characterIndices[char].push(i); // if the character already exists in the object, push the index to it
+      } else {
+        characterIndices[char] = [i]; // if the character does not exist in the count, give it an array with that index
+      }
     }
   }
   return characterIndices;
@@ -63,8 +55,10 @@ const letterPositions = function(sentence) {
 // Test code
 const results1 = letterPositions("hello");
 const results2 = letterPositions("My name is Adam Marsala");
+const results3 = letterPositions("lighthouse in the house");
 assertArraysEqual(results1["h"], [0]);
 assertArraysEqual(results1["l"], [2, 3]);
 assertArraysEqual(results1["e"], [1]);
 assertArraysEqual(results1["o"], [4]);
-assertArraysEqual(results2["a"], [3, 8, 10, 13, 16, 18]);
+assertArraysEqual(results2["a"], [4, 11, 13, 17, 20, 22]);
+assertArraysEqual(results3["h"], [3, 5, 15, 18]);

2b57d9b31c38dd807f8d0e3c0912caf6a115df20
Adam Marsala <adammarsala@hotmail.com>
2021-02-06 02:21:51 +0000

    Refactor of eqObjects to handle nested objects with recursiongit status

--- eqObjects.js
+++ eqObjects.js
@@ -32,8 +32,8 @@ const eqArrays = function(arr1, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  // list all the object keys into separate arrays
-  // use compareArrayLengths and use that return true/false value
+  // Calls eqArrays only when both objects are arrays..
+  /* Handles nested objects with recursion */
   
   const compareArrayLengths = function(arr1, arr2) { // compare array lengths
     return arr1.length === arr2.length ? true : false;
@@ -45,14 +45,24 @@ const eqObjects = function(object1, object2) {
     for (const key of Object.keys(object1)) { // Loop through object1 and compare key values with object2 key values
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // only if both are arrays
         result = eqArrays(object1[key], object2[key]); // call eqArrays
-      } else { // otherwise an array compared to something that is not an array will be false
-        console.log("object1 " + key + ": " + object1[key]);
-        console.log("object2 " + key + ": " + object2[key]);
-        result = object1[key] !== object2[key] ? false : true;
-      }
+      } else if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) { // only if both are NOT arrays
+        if (typeof object1[key] === "object" && typeof object2[key] === "object") { // only if both are objects (but not arrays)
+          // console.log("BOTH OBJECTS!");
+          // console.log("object1 " + key + ": " + object1[key]);
+          // console.log("object2 " + key + ": " + object2[key]);
+          result = eqObjects(object1[key], object2[key]); // recursive-case both elements are objects
+        } else { // regular values are being compared here OR one is an object and the other is NOT.. so result = false;
+          // console.log("object1 " + key + ": " + object1[key]);
+          // console.log("object2 " + key + ": " + object2[key]);
+          result = object1[key] !== object2[key] ? false : true;
+        }
+      } // if false result if found within looping through the keys
+      if (!result) return false;
     }
+    // console.log("all keys have been compared.....");
     return result;
   }
+  // console.log("the objects have different number of keys!");
   return false; // if they have a different number of keys
 };
 
@@ -82,3 +92,58 @@ assertEqual(result5, true);
 const cd2 = { c: "1", d: ["2", 3, 4] };
 const result6 = eqObjects(cd, cd2); // => false
 assertEqual(result6, false);
+
+
+// Test code for nested objects
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+}, {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+}), true); // => true
+
+assertEqual(eqObjects({
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file", // difference is here
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+}, {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "two files", // difference is here
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+}), false); // => false
+

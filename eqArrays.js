c11b177dd71c05bd63409d0d9e8107e7a2a16d34
Adam Marsala <adammarsala@hotmail.com>
2021-02-06 17:48:46 +0000

    Refactored equality checks for nested arrays with recursion

--- eqArrays.js
+++ eqArrays.js
@@ -15,19 +15,19 @@ const eqArrays = function(arr1, arr2) {
   };
 
   let result;
-  result = Array.isArray(arr1); // check if inputs are arrays
-  result = Array.isArray(arr2);
   result = compareArrayLengths(arr1, arr2);
   
   if (result) { // if result is still true at this point run the loop...
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-        return result; // if a mismatch is found exit the function
-      }
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        result = eqArrays(arr1[i], arr2[i]);
+        if (!result) return result; // if result of recursion was false, exit 
+      } else if (arr1[i] !== arr2[i]) { // comparison of values if both are not arrays or one is an array
+        return false;  // if a mismatch is found exit the function
+      } 
     }
   }
-  return result;
+  return result; 
 };
 
 
@@ -43,3 +43,11 @@ const eqArrays = function(arr1, arr2) {
 // assertEqual(eqArrays("[1, 2, 3]", [1, 2, 3]), false); // => should PASS
 // assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should PASS
 // assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), false); // => should PASS
+
+
+// TEST CODE FOR SUB-ARRAYS (NESTED) WITH RECURSION
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3, [1, [2], 3], 10], [4]], [[2, 3, [1, [2], 3], 10], [4]]), true);
+assertEqual(eqArrays([[2, 3, [1, [2], 3], 10], [4]], [[2, 3, [1, [20], 3], 10], [4]]), false);

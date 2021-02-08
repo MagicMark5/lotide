ac5579050c864b4c3a89706150047aae64ef2e15
Adam Marsala <adammarsala@hotmail.com>
2021-02-08 13:27:05 +0000

    assertEqual and head js now export their functions and have separate test files

--- assertEqual.js
+++ assertEqual.js
@@ -6,10 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(11, 1);
-assertEqual(false, 0);
-assertEqual("51", "5");
-assertEqual("WOO", "WOO");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(11, 1);
+assertEqual(false, 0);
+assertEqual("51", "5");
+assertEqual("WOO", "WOO");
